4ef0c9db1bca80bfa20a88b660ce9846fd94519b
gtpepi <gtpepi@hotmail.com>
2022-06-20 13:51:44 -0300

    Add Education

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,7 +2,32 @@ import React, { Component } from "react";
 
 class Education extends Component {
   render() {
-    return <div>Education</div>;
+    return (
+      <fieldset>
+        <button>Add</button>
+        <div>
+          <label htmlFor="school">School</label>
+          <input type="text" placeholder="School name" />
+        </div>
+        <div>
+          <label htmlFor="position">Course</label>
+          <input type="text" placeholder="Computer Science" />
+        </div>
+        <div>
+          <label htmlFor="startDate">From</label>
+          <input type="date" placeholder="dd/mm/yyyy" id="startDate" />
+        </div>
+        <div>
+          <label htmlFor="endDate">To</label>
+          <input type="date" placeholder="dd/mm/yyyy" id="endDate" />
+        </div>
+        <div>
+          <textarea className="description"></textarea>
+        </div>
+        <button>Save</button>
+        <button>Delete</button>
+      </fieldset>
+    );
   }
 }
 
