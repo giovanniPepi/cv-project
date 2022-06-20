bfa5f6ebb8b46ab75cf461f1f1a72b66b5292630
gtpepi <gtpepi@hotmail.com>
2022-06-20 13:48:55 -0300

    Add Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,32 @@ import React, { Component } from "react";
 
 class Experience extends Component {
   render() {
-    return <div>Experience</div>;
+    return (
+      <fieldset>
+        <button>Add</button>
+        <div>
+          <label htmlFor="company">Company</label>
+          <input type="text" placeholder="company name" />
+        </div>
+        <div>
+          <label htmlFor="position">Position</label>
+          <input type="text" placeholder="Sr React Developer" />
+        </div>
+        <div>
+          <label htmlFor="startDate">From</label>
+          <input type="date" placeholder="dd/mm/yyyy" id="startDate" />
+        </div>
+        <div>
+          <label htmlFor="endDate">To</label>
+          <input type="date" placeholder="dd/mm/yyyy" id="endDate" />
+        </div>
+        <div>
+          <textarea className="description"></textarea>
+        </div>
+        <button>Save</button>
+        <button>Delete</button>
+      </fieldset>
+    );
   }
 }
 
