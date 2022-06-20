49c536c2d2e20011cdfe6b613cad174a88c5a366
gtpepi <gtpepi@hotmail.com>
2022-06-20 13:31:54 -0300

    Update Personal component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import style from "./styles/App.css";
+import { Component } from "react";
+import Personal from "./components/Personal";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <form className="App" id="mainForm">
+        <fieldset>
+          <Personal />
+          <Skills />
+          <Experience />
+          <Education />
+        </fieldset>
+      </form>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Personal.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Personal extends Component {
+  render() {
+    return (
+      <fieldset>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" id="firstName" />
+          <button id="editFirstName">Edit </button>
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" id="lastName" />
+          <button id="editlastName">Edit </button>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="example@domain.com" />
+          <button id="editEmail">Edit </button>
+        </div>
+        <div>
+          <label htmlFor="phone">Phone</label>
+          <input type="tel" id="phone" placeholder="+55 19 989822222" />
+          <button id="editPhone">Edit </button>
+        </div>
+        <div>
+          <label htmlFor="gitHub">GitHub</label>
+          <input type="text" id="gitHub" placeholder="github.com/yourUser" />
+          <button id="editgitHub">Edit </button>
+        </div>
+        <div>
+          <label htmlFor="linkedin">LinkedIn</label>
+          <input type="tel" id="linkedin" placeholder="linkedin.com/in/user" />
+          <button id="editlinkedin">Edit </button>
+        </div>
+      </fieldset>
+    );
+  }
+}
+
+export default Personal;
