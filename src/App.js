7070e16a7448abe9f0e8fbc71ad9a92d667f4ff2
giovanniPepi <gtpepi@hotmail.com>
2022-06-21 15:07:11 -0300

    Update Skills.js, update App to add new skills when clicking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import uniqid from "uniqid";
 import Personal from "./components/Personal";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
@@ -6,15 +7,44 @@ import Education from "./components/Education";
 import style from "./styles/App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      skillsIds: [],
+      educationIds: [],
+      experienceIds: [],
+    };
+  }
+
+  // creates a new instace of the element inside the state, which is then rendered
+  handleClick(type) {
+    this.setState((prevState) => {
+      return {
+        [type]: [...prevState[type], uniqid()],
+      };
+    });
+  }
+  handleDelete() {}
+
   render() {
+    const skills = this.state.skillsIds.map((id) => (
+      <Skills key={id} id={id} handleDelete={this.handleDelete} />
+    ));
     return (
       <main className="App" id="mainForm">
         <section className="mainSection">
           <h2>CV Generator</h2>
-          <h3>General Info</h3>
+          <h3 className="sectionTitle">General Info</h3>
           <Personal />
-          <h3>Skills</h3>
-          <Skills />
+          <h3 className="sectionTitle">Skills</h3>
+          {skills}
+          <button
+            className="addBtn"
+            onClick={() => this.handleClick("skillsIds")}
+          >
+            Add
+          </button>
           <h3>Experience</h3>
           <Experience />
           <h3>Education</h3>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,36 +1,72 @@
 import React, { Component } from "react";
 
 class Skills extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editMode: true,
+      skill: "",
+      description: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState((prevState) => {
+      return { editMode: !prevState.editMode };
+    });
+  }
+
   render() {
+    const { editMode, skill, description } = this.state; //destructuring
+    const { id, handleDelete } = this.props; // receives from App
+
+    if (!editMode) {
+      // print logic
+    }
+
     return (
-      <fieldset>
-        <div>
-          <label htmlFor="skill1">Skill 1</label>
-          <input type="text" id="skill1" />
-          <button id="editSkill1">Edit</button>
-          <button id="delteSkill1">Delete</button>
+      <form className="personalForm" action="" onSubmit={this.handleSubmit}>
+        <div className="innerFormDiv">
+          <label htmlFor="skill">
+            <p>Skill</p>
+            <input
+              type="text"
+              name="skill"
+              value={skill}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+
+          <label htmlFor="description">
+            <p>Description</p>
+            <input
+              type="text"
+              name="description"
+              value={description}
+              onChange={this.handleChange}
+            />
+          </label>
         </div>
-        <div>
-          <label htmlFor="skill2">Skill 2</label>
-          <input type="text" id="skill2" />
-          <button id="editSkill2">Edit</button>
-          <button id="delteSkill2">Delete</button>
-        </div>
-        <div>
-          <label htmlFor="skill3">Skill 3</label>
-          <input type="text" id="skill3" />
-          <button id="editSkill3">Edit</button>
-          <button id="delteSkill3">Delete</button>
-        </div>
-        <div>
-          <label htmlFor="skill4">Skill 4</label>
-          <input type="text" id="skill4" />
-          <button id="editSkill4">Edit</button>
-          <button id="delteSkill4">Delete</button>
-        </div>
-        <button>Add Skills</button>
-        <button>Save</button>
-      </fieldset>
+        <button className="formBtn">Save</button>
+        <button
+          className="formBtn skillsBtn"
+          type="submit"
+          onClick={() => handleDelete("skillsIds", id)}
+        >
+          Delete
+        </button>
+      </form>
     );
   }
 }
