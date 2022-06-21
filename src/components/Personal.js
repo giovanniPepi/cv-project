a2e1ac7fe8022c3537bdb1a36cbcfde8515fc749
gtpepi <gtpepi@hotmail.com>
2022-06-21 13:13:13 -0300

    Create Textfield, update Personal

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,36 +1,76 @@
 import React, { Component } from "react";
+import Textfield from "./Textfield";
 
 class Personal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editMode: true,
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      github: "",
+      linkedin: "",
+    };
+  }
   render() {
+    // destructuring the state
+
+    const { editMode, firstName, lastName, email, phone, github, linkedin } = this.state
+
+    // constructed when edit mode off
+    if (!editMode) {
+      return (
+        <Textfield
+          firstName={firstName}
+          lastName={lastName}
+          email={email}
+          phone={phone}
+          github={github}
+          linkedin={linkedin}
+          handleEdit={''}
+          />       
+      )
+    }
+    
+    // constructed when edit mode is on
     return (
-      <fieldset>
+         return (
+      <fieldset className="textfield">
         <div>
-          <label htmlFor="firstName">First Name</label>
+          <label htmlFor="firstName">First Name: </label>
+          {props.firstName}
           <input type="text" id="firstName" />
           <button id="editFirstName">Edit </button>
         </div>
         <div>
           <label htmlFor="lastName">Last Name</label>
+          {props.lastName}
           <input type="text" id="lastName" />
           <button id="editlastName">Edit </button>
         </div>
         <div>
           <label htmlFor="email">Email</label>
+          {props.email}
           <input type="email" id="email" placeholder="example@domain.com" />
           <button id="editEmail">Edit </button>
         </div>
         <div>
           <label htmlFor="phone">Phone</label>
+          {props.phone}
           <input type="tel" id="phone" placeholder="+55 19 989822222" />
           <button id="editPhone">Edit </button>
         </div>
         <div>
           <label htmlFor="gitHub">GitHub</label>
+          {props.github}
           <input type="text" id="gitHub" placeholder="github.com/yourUser" />
           <button id="editgitHub">Edit </button>
         </div>
         <div>
           <label htmlFor="linkedin">LinkedIn</label>
+          {props.linkedin}
           <input type="tel" id="linkedin" placeholder="linkedin.com/in/user" />
           <button id="editlinkedin">Edit </button>
         </div>
@@ -38,7 +78,7 @@ class Personal extends Component {
           <button>Save</button>
         </div>
       </fieldset>
-    );
+    )
   }
 }
 
---
+++ src/components/Textfield.js
@@ -0,0 +1,39 @@
+// returns text areas tailored to each section based on the props received
+function Textfield(props) {
+  if (props.firstName) {
+    return (
+      <fieldset className="textfield">
+        <div>
+          <span htmlFor="firstName">First Name: </span>
+          {props.firstName}
+        </div>
+        <div>
+          <span htmlFor="lastName">Last Name</span>
+          {props.lastName}
+        </div>
+        <div>
+          <span htmlFor="email">Email</span>
+          {props.email}
+        </div>
+        <div>
+          <span htmlFor="phone">Phone</span>
+          {props.phone}
+        </div>
+        <div>
+          <span htmlFor="gitHub">GitHub</span>
+          {props.github}
+        </div>
+        <div>
+          <span htmlFor="linkedin">LinkedIn</span>
+          {props.linkedin}
+        </div>
+        <div>
+          <button>Save</button>
+        </div>
+      </fieldset>
+    );
+  } else if (props.skill) {
+  } else if (props.company) {
+  } else {
+  }
+}
